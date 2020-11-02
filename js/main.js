102c3f389fac725bffec93a9681e78294ed7a45f
millet <millet.mathieu.0@gmail.com>
2020-11-02 16:20:43 +0100

    mode sombre operationel

--- js/main.js
+++ js/main.js
@@ -42,3 +42,24 @@ sr.reveal(".citation", {
     distance: "60px",
     duration: 3000
 })
+
+//////////////DARK MODE/////////////////
+
+
+// On sélectionne le bouton
+const btn = document.querySelector(".switch_theme_color");
+// On récupère le thème dans le link css avec l'id #theme-link
+const theme = document.querySelector("#theme-link");
+
+// On active la fonction au click sur le bouton
+btn.addEventListener("click", function () {
+  // Si l'URL contient "css/styles_light.css"
+  if (theme.getAttribute("href") == "css/styles_light.css") {
+    // ... Alors on bascule sur "css/styles_dark.css"
+    theme.href = "css/styles_dark.css";
+    // Sinon...
+  } else {
+    // ...on passe en mode clair
+    theme.href = "css/styles_light.css";
+  }
+});
